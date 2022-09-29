8af362fc6a21697a4233558bc081368bf8779f63
Aczz1 <yzz31035@163.com>
2022-09-29 13:25:31 +0800

    http status 418

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Nice work,mafeifei!!!");
 });
+
+app.get("demo", (req, res) => {
+  res.set("X-ful-stack", "4life");
+  res.status(418);
+  res.send("I profer coffee");
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
